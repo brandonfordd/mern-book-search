a26cfb661e78b0d510c153986b32329d8ca8a598
Brandon Ford <Brandon.ford.617@gmail.com>
2021-07-14 17:59:58 -0400

    added sendfile path in server for index

--- server/server.js
+++ server/server.js
@@ -3,18 +3,21 @@ const path = require('path');
 const db = require('./config/connection');
 // removed routes to be replaced with apolloserver
 const { ApolloServer } = require('apollo-server-express');
+//import our typeDefs and resolvers
 const { typeDefs, resolvers } = require('./Schemas');
 const { authMiddleware } = require('./utils/auth');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+//create new Apollo server and pass in our schema data
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: authMiddleware
 });
 
+//integrate our Apollo server with Express application as middleware
 server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
@@ -24,6 +27,11 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
 
 db.once('open', () => {
   app.listen(PORT, () => {
