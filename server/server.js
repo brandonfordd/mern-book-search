9c180f3d00de235f8b7653b6ed8962cafc6eed65
Brandon Ford <Brandon.ford.617@gmail.com>
2021-07-14 21:35:39 -0400

    added mongoose require method

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const { authMiddleware } = require('./utils/auth');
+const mongoose = require("mongoose");
 
 const path = require('path');
 const db = require('./config/connection');
